f6128db66d0fee641ca49274eac3197b9f0e4b25
Phoenix <80379347+Prime-Phoenix@users.noreply.github.com>
2023-05-16 15:13:48 -0500

    Update Race.py

--- Race.py
+++ Race.py
@@ -1,9 +1,14 @@
+import random
+from colorama import Fore
+
 class Car:
     def __init__(self, speed=10, health=100):
         self.speed = speed
         self.health = health
         self.progress = 0
         self.sprite = 'CAR'
+        self.is_spiked = [False, 0]
+        self.is_overheated = [False, 0]
 
     def change_health(self, amount):
         self.health += self.health * (amount / 100)
@@ -17,14 +22,21 @@ class Car:
             self.sprite = 'DED'
         return self.progress
 
+    def spike(self):
+        self.is_spiked = [True, 10]
 
-from colorama import Fore
+    def overheat(self):
+        self.is_overheated = [True, 3]
+
+
+
 
 
 class Race:
     def __init__(self, p1: Car, p2: Car):
         self.p1 = p1
         self.p2 = p2
+        self.call_dict = {1: V8, 2: chassis, 3: fix, 4: repair, 8: spikes, 9: hammer, 10: explosive, 11: overheat}
 
     def has_p1_won(self):
         if self.p2.health <= 0:
@@ -44,10 +56,48 @@ class Race:
             return True
         return False
 
-    def update_progress(self, player, card=1):
-        if player == 1:
+    def update_progress(self, turn, card=1):
+        # spikes p1
+        if self.p1.is_spiked[0]:
+            self.p1.change_speed(-p1.is_spiked[1])
+            if self.p1.is_spiked[1] >= 20:
+                self.p1.is_spiked = [False, 0]
+            else:
+                self.p1.is_spiked[1] += 5
+
+        # overheat p1
+        if self.p1.is_overheated[0]:
+            if self.p1.is_overheated[1] <= 0:
+                self.p1.is_overheated = [False, 0]
+            else:
+                self.p1.change_speed(10)
+                self.p1.is_overheated[1] -= 1
+
+        # spikes p2
+        if self.p2.is_spiked[0]:
+            self.p2.change_speed(-p2.is_spiked[1])
+            if self.p2.is_spiked[1] >= 20:
+                self.p2.is_spiked = [False, 0]
+            else:
+                self.p2.is_spiked[1] += 5
+
+        # overheat p2
+        if self.p2.is_overheated[0]:
+            if self.p2.is_overheated[1] <= 0:
+                self.p2.is_overheated = [False, 0]
+            else:
+                self.p2.change_speed(10)
+                self.p2.is_overheated[1] -= 1
+
+        if turn % 2 == 1:
+            # call card
+            self.call_dict[card](1)
+            # update
             self.p1.update_progress()
-        if player == 2:
+        if turn % 2 == 0:
+            # call card
+            self.call_dict[card](2)
+            # update
             self.p2.update_progress()
 
     def print_progress(self):
@@ -59,3 +109,60 @@ class Race:
             print(Fore.RED + "P2 WON!")
 
         print(Fore.WHITE, end='')
+
+    def V8(self, player):
+        if player == 1:
+            self.p1.change_speed(20)
+        elif player == 2:
+            self.p2.change_speed(20)
+
+    def chassis(self, player):
+        if player == 1:
+            self.p1.change_speed(30)
+            self.p1.change_health(-10)
+        elif player == 2:
+            self.p2.change_speed(30)
+            self.p2.change_health(-10)
+
+    def fix(self, player):
+        if player == 1:
+            self.p1.is_spiked = [False, 0]
+            self.p1.is_overheated = [False, 0]
+        elif player == 2:
+            self.p2.is_spiked = [False, 0]
+            self.p2.is_overheated = [False, 0]
+
+    def repair(self, player):
+        if player == 1:
+            self.p1.change_health(20)
+        elif player == 2:
+            self.p2.change_health(20)
+
+    def spikes(self, player):
+        if player == 1:
+            self.p2.spike()
+        elif player == 2:
+            self.p1.spike()
+
+    def hammer(self, player):
+        if player == 1:
+            self.p2.change_health(-20)
+        elif player == 2:
+            self.p1.change_health(-20)
+
+    def explosive(self, player):
+        if player == 1:
+            self.p2.change_health(-30)
+            if random.choice([True, False]):
+                self.p1.change_health(-10)
+        if player == 2:
+            self.p1.change_health(-30)
+            if random.choice([True, False]):
+                self.p2.change_health(-10)
+                
+    def overheat(self, player):
+        if player == 1:
+            self.p2.overheat()
+        if player == 2:
+            self.p1.overheat()
+            
