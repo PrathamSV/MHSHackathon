dacf2676081325a0f2ed36516acaa1f064f17536
Codemage-God <121194130+Codemage-God@users.noreply.github.com>
2023-05-16 14:20:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,63 @@
 # 1-7: Upgrades, 8-14: Traps
-cards = {1:"V8 Engine", 2:"Carbonfiber Chassis", 3:"Fix", 4:"Repair", 5:"Camera", 6:"Spare Parts", 7:"Asset", 8: "Spikes", 9:"Weight", 10:"Explosive", 11:"Overheat", 12:"Reporter", 13:"Speed Limit", 14:"Liability"}
+import random
 
-# V8Engine 20% increase in speed
-# Lightweight Chassis 30% increase in speed, 10% decrease in HP
-# Fix removes DoT
-# Repair 20% increase in health
-# See opponent's hand
-# Spare Parts Refresh deck
-'''Asset increase in all initial effects by +10%. Doesn't work with Camera. If used with Spare Parts,
-    allows you to play refreshed card. With Fix, protects from all traps'''
+cards = {1: "V8 Engine", 2: "Carbon-fiber Chassis", 3: "Fix", 4: "Repair", 5: "Camera", 6: "Spare Parts", 7: "Asset",
+         8: "Spikes", 9: "Weight", 10: "Explosive", 11: "Overheat", 12: "Reporter", 13: "Speed Limit", 14: "Liability"}
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 1, 2, 3, 4, 5, 6,
+        7, 8, 9, 10, 11, 12, 13, 14]
+random.shuffle(deck)
+discard = []
+turn = 1
+hand1 = []  # P1's hand
+hand2 = []  # P2's hand
 
 
-# Spikes 10% reduction in speed, increases by 5% every turn
-# Hammer 20% reduction in health
-# Explosive 30% reduction in health, but chance of 10% reduction of own health
-# Overheat 30% reduction in speed but effect reduces by 10% every turn
-# Reporter Remove rando card from opp
-'''Speed Limit If opp speed is above random limit, reduce speed by 30%. 50% chance to reduce 
-    your speed by 10% if you're above limit.'''
-# Liability increases all initial effects by +10%. Doesn't increase recurring spikes effect and overheat effect
+# Shuffles deck when cards are done
+def shuffle():
+    global deck
+    global discard
+    if len(deck) == 0:
+        deck = discard
+        random.shuffle(deck)
+        discard = []
+
+
+# Draws a card for hand
+def draw():
+    global deck
+    x = deck.pop()
+    shuffle()
+    return x
+
+
+# Plays a card from hand
+def play():
+    choice = "0"
+    while choice != "1" and choice != "2" and choice != "3":
+        choice = input("What card do you want to play? (Select a number from 1-3): ")
+    global hand1
+    global hand2
+    global turn
+    x = 0
+    if turn % 2 == 1:
+        x = hand1.pop(int(choice))
+        discard.append(x)
+        hand1.append(draw())
+    else:
+        x = hand2.pop(int(choice))
+        discard.append(x)
+        hand2.append(draw())
+    turn += 1
+    return x
+
+
+for i in range(3):
+    hand1.append(draw())
+for i in range(3):
+    hand2.append(draw())
+race = Race()
+while True:
+    card = play()
+    race.update_progress(2 if turn % 2 == 0 else 1, card)
+    if race.has_p1_won() or race.has_p2_won():
+        break
