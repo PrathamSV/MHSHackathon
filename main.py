1aa3f730d111ceac191d1f52fc7bf119d9fe4022
Phoenix <80379347+Prime-Phoenix@users.noreply.github.com>
2023-05-16 16:06:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 # 1-7: Upgrades, 8-14: Traps
 cards = {1:"V8 Engine", 2:"Carbonfiber Chassis", 3:"Fix", 4:"Repair", 5:"Camera", 6:"Spare Parts", 7:"Asset", 8: "Spikes", 9:"Weight", 10:"Explosive", 11:"Overheat", 12:"Reporter", 13:"Speed Limit", 14:"Liability"}
 
-# V8Engine 20% increase in speed
-# Lightweight Chassis 30% increase in speed, 10% decrease in HP
-# Fix removes DoT
-# Repair 20% increase in health
-# Camera See opponent's hand
-# Spare Parts Refresh deck
+# V8Engine: +20 speed
+# Lightweight Chassis: +30 speed, -10 health
+# Fix: removes DoT
+# Repair: +20 health
+# Camera: See opponent's hand
+# Spare Parts: Refresh deck
 '''Asset increase in all initial effects by +10%. Doesn't work with Camera. If used with Spare Parts,
     allows you to play refreshed card. With Fix, protects from all traps'''
 
-# Spikes 10% reduction in speed, increases by 5% every turn until ~40% decrease in total
-# Hammer 20% reduction in health
-# Explosive 30% reduction in health, but chance of 10% reduction of own health
-'''Overheat 30% reduction in speed but effect reduces by 10% every turn (so, after 3 turns, 
-    the speed is unaffected & back to normal)'''
-# Reporter Remove rando card from opp
-'''Speed Limit If opp speed is above random limit, reduce speed by 30%. 50% chance to reduce 
-    your speed by 10% if you're above limit.'''
+# Spikes: -10 speed, -15, and -20 on subsequent turns
+# Hammer: -20 health
+# Explosive: -30 health, but, 50% chance on -10 on own health
+# Overheat: -25 health, then -10, and -5 on subsequent turns
+# Reporter: Remove rando card from opp
+'''Speed Limit If opp speed is above random limit, -30 speed. 50% chance to reduce 
+    your -10 of your speed if you're above limit.'''
 # Liability increases all initial effects by +10%. Doesn't increase recurring spikes effect and overheat effect
