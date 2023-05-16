9a731e132a00ea5fd05293c92266c4cc7bac7bab
Phoenix <80379347+Prime-Phoenix@users.noreply.github.com>
2023-05-16 15:36:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ cards = {1:"V8 Engine", 2:"Carbonfiber Chassis", 3:"Fix", 4:"Repair", 5:"Camera"
 # Lightweight Chassis 30% increase in speed, 10% decrease in HP
 # Fix removes DoT
 # Repair 20% increase in health
-# See opponent's hand
+# Camera See opponent's hand
 # Spare Parts Refresh deck
 '''Asset increase in all initial effects by +10%. Doesn't work with Camera. If used with Spare Parts,
     allows you to play refreshed card. With Fix, protects from all traps'''
 
-
-# Spikes 10% reduction in speed, increases by 5% every turn
+# Spikes 10% reduction in speed, increases by 5% every turn until ~40% decrease in total
 # Hammer 20% reduction in health
 # Explosive 30% reduction in health, but chance of 10% reduction of own health
-# Overheat 30% reduction in speed but effect reduces by 10% every turn
+'''Overheat 30% reduction in speed but effect reduces by 10% every turn (so, after 3 turns, 
+    the speed is unaffected & back to normal)'''
 # Reporter Remove rando card from opp
 '''Speed Limit If opp speed is above random limit, reduce speed by 30%. 50% chance to reduce 
     your speed by 10% if you're above limit.'''
