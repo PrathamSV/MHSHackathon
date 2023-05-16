ff60d4fbb206b68a8bf0d63bd76fe8939b699d4a
Phoenix <80379347+Prime-Phoenix@users.noreply.github.com>
2023-05-16 12:41:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 1-7: Upgrades, 8-14: Traps
 cards = {1:"V8 Engine", 2:"Carbonfiber Chassis", 3:"Fix", 4:"Repair", 5:"Camera", 6:"Spare Parts", 7:"Asset", 8: "Spikes", 9:"Weight", 10:"Explosive", 11:"Overheat", 12:"Reporter", 13:"Speed Limit", 14:"Liability"}
 
-# V8Engine 15% increase in speed
+# V8Engine 20% increase in speed
 # Lightweight Chassis 30% increase in speed, 10% decrease in HP
 # Fix removes DoT
 # Repair 20% increase in health
