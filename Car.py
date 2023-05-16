52dd5e11fbe87c25d308c5c08a6a5734876a9f70
Phoenix <80379347+Prime-Phoenix@users.noreply.github.com>
2023-05-16 12:12:48 -0500

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,10 +1,10 @@
 class Car:
-    def __init__(self, speed, health):
+    def __init__(self, speed=50, health=100):
         self.speed = speed
         self.health = health
 
     def change_health(self, amount):
-        self.health += amount
+        self.health += self.health * (amount/100)
 
     def change_speed(self, amount):
-        self.speed += amount
+        self.speed += self.speed * (amount/100)
