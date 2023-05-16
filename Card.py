162973ffca0df96445abe48eb54cf248b63a7748
Codemage-God <121194130+Codemage-God@users.noreply.github.com>
2023-05-16 15:46:21 -0500

    Update Card.py

--- Card.py
+++ Card.py
@@ -29,30 +29,31 @@ def draw():
     shuffle()
     return x
 
+
 def view(player, camera):
     if player == 1:
-         print("Here are your cards: " if not camera else "These are your opponent's cards:")
-         if len(hand1) == 3:
-              print("1." + cards[hand1[0]] + ", 2." + cards[hand1[1]] + ", 3." + cards[hand1[2]])
-         else:
-              print("1." + cards[hand1[0]] + ", 2." + cards[hand1[1]])
+        print("Here are your cards: " if not camera else "These are your opponent's cards:")
+        if len(hand1) == 3:
+            print("1." + cards[hand1[0]] + ", 2." + cards[hand1[1]] + ", 3." + cards[hand1[2]])
+        else:
+            print("1." + cards[hand1[0]] + ", 2." + cards[hand1[1]])
     else:
-         print("Here are your cards: " if not camera else "These are your opponent's cards:")
-         if len(hand2) == 3:
-              print("1." + cards[hand2[0]] + ", 2." + cards[hand2[1]] + ", 3." + cards[hand2[2]])
-         else:
-              print("1." + cards[hand2[0]] + ", 2." + cards[hand2[1]])
+        print("Here are your cards: " if not camera else "These are your opponent's cards:")
+        if len(hand2) == 3:
+            print("1." + cards[hand2[0]] + ", 2." + cards[hand2[1]] + ", 3." + cards[hand2[2]])
+        else:
+            print("1." + cards[hand2[0]] + ", 2." + cards[hand2[1]])
+
 
 # Plays a card from hand
 def play():
-    view(turn % 2, false)
+    global turn
+    view(turn % 2, False)
     choice = "0"
     while choice != "1" and choice != "2" and choice != "3":
         choice = input("What card do you want to play? (Select a number from 1-3): ")
     global hand1
     global hand2
-    global turn
-    x = 0
     if turn % 2 == 1:
         x = hand1.pop(int(choice))
         discard.append(x)
@@ -61,33 +62,38 @@ def play():
         discard.append(x)
     turn += 1
     if x == 5:
-         view(turn % 2 + 1, true)
-    if x == 6:
-         view(turn % 2, false)
-         y = "0"
-         while y != "1" and y != "2": input("Discard one or both cards (Select a number from 1-2):")
-         if y == "1":
-              z = "0"
-              while z != "1" and z != "2": input("What card do you want to discard? (Select a number from 1-2): ")
-              if turn % 2 == 1:
-                  discard.append(hand1.pop(int(choice)))
-                  hand1.append(draw())
-              else:
-                  discard.append(hand2.pop(int(choice)))
-                  hand2.append(draw())
-         else:
-              if turn % 2 == 1:
-                  hand1 = []
-                  hand1.append(draw())
-                  hand1.append(draw())
-              else:
-                  hand2 = []
-                  hand2.append(draw())
-                  hand2.append(draw())
+        view(turn % 2 + 1, True)
+    elif x == 6:
+        view(turn % 2, False)
+        y = "0"
+        while y != "1" and y != "2":
+            input("Discard one or both cards (Select a number from 1-2):")
+        if y == "1":
+            z = "0"
+            while z != "1" and z != "2":
+                input("What card do you want to discard? (Select a number from 1-2): ")
+            if turn % 2 == 1:
+                discard.append(hand1.pop(int(choice)))
+                hand1.append(draw())
+            else:
+                discard.append(hand2.pop(int(choice)))
+                hand2.append(draw())
+        else:
+            if turn % 2 == 1:
+                hand1 = [draw(), draw()]
+            else:
+                hand2 = [draw(), draw()]
+    elif x == 12:
+        if turn % 2 == 1:
+            hand2.pop(random.randint(0, 2))
+            hand2.append(draw())
+        else:
+            hand1.pop(random.randint(0, 2))
+            hand1.append(draw())
     if turn % 2 == 1:
-         hand1.append(draw())
+        hand1.append(draw())
     else:
-         hand2.append(draw())
+        hand2.append(draw())
     return x
 
 
